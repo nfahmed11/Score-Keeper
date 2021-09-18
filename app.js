b93d9284282c29ac629bc1d877ca66b7d4277127
nfahmed11 <nfahmed11@gmail.com>
2021-09-17 23:06:39 -0500

    html layout

---
+++ app.js
@@ -0,0 +1,8 @@
+const teamNames = document.querySelector("#teamnames");
+const button = document.querySelector("button");
+
+button.addEventListener('click', function(e) {
+    e.preventDefault();
+    console.log("button clicked")
+
+})
