e96e3d83dce5369a7a7c2b581e14171df67e912f
nfahmed11 <nfahmed11@gmail.com>
2021-09-22 18:44:38 -0500

    js changes

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
 const teamNames = document.querySelector("#teamnames");
 const button = document.querySelector("button");
 const numberofteams = document.querySelector("#Numofteams");
-const h2teamname = document.querySelector("#teamname");
-const teamnames = document.querySelector("#teamnames");
+const listtitle = document.querySelector("#listtitle");
+const list = document.querySelector("#list");
 
-//listen for user input for number of teams and use input to create team name input
+//listen for user input for number of teams and use input to show team name and submit button
 numberofteams.addEventListener("input", function () {
+  //remove everything prevoiusly loaded first
+  list.innerHTML = "";
+
+  //get user input value
   const userinputofnumofteam = numberofteams.value;
   console.log(userinputofnumofteam);
+  listtitle.style.display = "flex";
+  button.style.display = "flex";
 
-  h2teamname.innerHTML = "Team Names ";
-  h2teamname.classList.add(<i class="fas fa-list-ol"></i>);
+  //create team name input form
+  for (let i = 0; i < userinputofnumofteam; i++) {
+    // create new form according to user input
+    const newForm = document.createElement("input");
+    newForm.setAttribute("type", "text");
+    newForm.setAttribute("id", "form" + i);
+    newForm.setAttribute("placeholder", `Team ${i + 1}`);
 
-  //create team name input
-  for (let i = 0; i < userinputofnumofteam; i++) {}
+    //push/append input form to html
+    list.appendChild(newForm);
+    list.appendChild(document.createElement("br"));
+  }
 });
 
+// submit button - collect user input on team names
+//make a button for each team that increases/decreases points
 button.addEventListener("click", function (e) {
   e.preventDefault();
-  console.log("button clicked");
 });
+
+// track points to see who wins (if else statement boolean?)
